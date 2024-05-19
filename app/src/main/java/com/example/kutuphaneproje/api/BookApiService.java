93bff46cc5df53888c16bb2329718701a022a271
Batuhan Enes Kırcalı <79521748+enesQwerty3@users.noreply.github.com>
2024-05-20 00:07:22 +0300

    Update BookApiService.java

--- app/src/main/java/com/example/kutuphaneproje/api/BookApiService.java
+++ app/src/main/java/com/example/kutuphaneproje/api/BookApiService.java
@@ -6,7 +6,7 @@ import retrofit2.http.Headers;
 import retrofit2.http.Query;
 
 public interface BookApiService {
-    @Headers("key:AIzaSyCEznZd29cRwGer1hGZz3XGfzYZU6QbYrI")
+    @Headers("key:your_apikey")
     @GET("books/v1/volumes")
     Call<Books> getSearchRequest(@Query(value = "q") String query, @Query("startIndex") int startIndex);
 }
